68683b7150acbea7c21e84e22859a1ac13fcdeba
fernandoxu <fernandoxu9@gmail.com>
2022-09-28 16:06:13 +0800

    ⏱️ useInterval

---
+++ lib/useInterval.tsx
@@ -0,0 +1,22 @@
+import { useEffect, useRef } from 'react';
+
+const useInterval = (callback: () => void, delay?: number | null) => {
+  const savedCallback = useRef<() => void>();
+
+  useEffect(() => {
+    savedCallback.current = callback;
+  });
+
+  useEffect(() => {
+    function tick() {
+      savedCallback.current?.();
+    }
+
+    if (delay !== null) {
+      const id = setInterval(tick, delay);
+      return () => clearInterval(id);
+    }
+  }, [delay]);
+};
+
+export default useInterval;
